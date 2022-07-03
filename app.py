4077667522b6579a946b7a7bc4a454651765b604
BC-Tong <85863509+BC-Tong@users.noreply.github.com>
2022-07-04 02:13:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,17 @@ def register_action():
     con.close()
     return '註冊成功'
 
+def login_check(userAccount,userPassword):
+    con = sqlite3.connect('db_MISproject.db')
+    cur = con.cursor()
+    querydata = cur.execute("SELECT * FROM user WHERE userAccount=? AND userPassword=?",(userAccount,userPassword))
+    results = cur.fetchall()
+    con.close
+    if results:
+        return True
+    else:
+        return False
+
 @app.route('/', methods=['GET','POST'])
 def handle_call():
     return "Successfully Connected!!!"
@@ -34,17 +45,19 @@ def register_getData():
     if request.method=='POST':
         return register_action()
     
+@app.route('/login', methods=['GET','POST'])
+def login():
+    userAccount  = int(request.form['Account'])
+    userPassword = int(request.form['Password'])
+    if request.method=='POST':
+        result = login_check(userAccount,userPassword)
+        if result = True:
+            return 1        #1表示登入成功
+        else:
+            return 0        #0表示登入失敗
+    
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
-
-    
-'''
-@app.route('/save', methods=['POST'])
-def handle_save():
-    account=str(request.form['num1'])
-    password=str(request.form['num2'])
-    return (account,password)
-'''
 
 
 
