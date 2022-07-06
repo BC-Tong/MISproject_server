7663349a78d5dfd89fdf81e4f90f111ab5127f62
BC-Tong <85863509+BC-Tong@users.noreply.github.com>
2022-07-07 00:14:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import sqlite3, os, sys
+import re
 
 os.path.join(__file__, 'MISProject_database.db')
 print(os.path.abspath(os.path.dirname(__file__)))
@@ -22,8 +23,12 @@ def register_action():
         return '請輸入username'
     elif not email:
         return '請輸入email'
+    elif email != re.compile('\w+@\w+\.[a-z]{3}')
+        return 'email格式錯誤'
     elif not password:
         return '請輸入password'
+    elif len(password)<4:
+        return '密碼必須大於4碼' 
     elif not sex:
         return '請輸入性別'
     elif not birthdate:
@@ -84,9 +89,6 @@ def login():
         else:
             return '查無此會員'
             
-    else:
-        login_check('jack90325@gmail.com', 'jack')
-        return login_action('jack90325@gmail.com')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -106,92 +108,7 @@ if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5050, debug=True)
 
 
-'''
-from typing import Text
-import flask
-from flask import Flask
-from flask import request
 
-from flask import flash
-from flask import redirect
-from flask import url_for
-from flask import session
-from flask import jsonify
-
-import sqlite3
-import os
-
-
-app = flask.Flask(__name__)
-
-def register_action():
-    #取得Data
-    userName  = request.form['Name']
-    userPassword = request.form['Password']
-    userMail = request.form['Mail']
-    userGender = request.form['Gender']
-    userBirthday = request.form['Birthday']
-    #連接db_MISproject.db
-    con = sqlite3.connect('db_MISproject.db')
-    cur = con.cursor()
-    
-    #檢查email
-    cur.execute('SELECT * FROM User_table WHERE `UserMail` = "{userMail}"')
-    queryresult = cur.fetchall()
-    if queryresult:
-        return 'email已存在，請使用另一個email'
-    #檢查userName
-    cur.execute('SELECT * FROM User_table WHERE `UserName` = "{userName}"')
-    queryresult = cur.fetchall()
-    if queryresult:
-        return '該名稱已被使用，請使用另一個名稱'
-    #將資料放入User_table中
-    cur.execute("INSERT INTO User_table(UserName,UserPassword,UserMail,UserGender,UserBirthday) values(?,?)",(userName,userPassword,userMail,userGender,userBirthday))
-    com.commit()
-    con.close()
-    return '註冊成功'
-
-def login_check(userName,userPassword):
-    con = sqlite3.connect('db_MISproject.db')
-    cur = con.cursor()
-    querydata = cur.execute("SELECT * FROM user WHERE userAccount=? AND userPassword=?",(userName,userPassword))
-    results = cur.fetchall()
-    con.close
-    if results:
-        return True
-    else:
-        return False
-
-@app.route('/', methods=['GET','POST'])
-def handle_call():
-    return "Successfully Connected!!!"
-
-@app.route('/register', methods=['GET','POST'])
-def register_getData():
-    if request.method=='POST':
-        a="123"
-        
-        return register_action()
-        #return jsonify(userName=userName ,userPassword=userPassword ,userMail=userMail,userGender=userGender,userBirthday=userBirthday)
-        #return a
-    
-    
-@app.route('/login', methods=['GET','POST'])
-def login():
-    userName  = int(request.form['Name'])
-    userPassword = int(request.form['Password'])
-    
-    if request.method=='POST':
-        result = login_check(userName,userPassword)
-        if result:
-            return "1"        #1表示登入成功
-        else:
-            return "0"        #0表示登入失敗
-    
-    
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
-'''
 
 
 
