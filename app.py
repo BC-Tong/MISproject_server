9a5962a523bf790cf52deb219303af9e48786c53
BC-Tong <85863509+BC-Tong@users.noreply.github.com>
2022-07-27 00:11:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -125,46 +125,18 @@ def print():
         return '{} {} {} {} {} {} {} {} {} {} {} {} {} {} {}'.format(result[0],result[1],result[2],result[3],result[4],result[5],result[6],result[7],result[8],result[9],result[10],result[11],result[12],result[13],result[14])
     else:
         return "error-menuName not found in db"
-'''下午版本    
-@app.route('/record', methods=['GET', 'POST'])
-def record():
-    if(request.method == 'POST'):
-        score = request.form['score']
-        menuname = request.form['menuname']
-        
-        con =sqlite3.connect('MISProject_database.db')
-        cur = con.cursor()
-        cur.execute(f"INSERT INTO Record_table (`user_id`, `menuname`, `score`, `finish_time`) VALUES( 1,'{menuname}', '{score}', date('now'))")
-        con.commit()
-        con.close()
-        return "successful insert"
-        
-@app.route('/printrecord', methods=['GET', 'POST'])
-def printrecord():
-    con =sqlite3.connect('MISProject_database.db')
-    cur = con.cursor()
-    querydata = cur.execute(f'SELECT user_id,menuname,score,finish_time FROM Record_table WHERE record_id = (SELECT MAX(record_id)  FROM Record_table)')
-    result = querydata.fetchone()
-    con.close()
-    
-    menucarl = '100大卡' 
-    
-    if result:
-        return '{} {} {} {}'.format(result[3],result[1],menucarl,result[2])
-    else:
-        return "error- not found data in db"    
-'''
+
 @app.route('/record', methods=['GET', 'POST'])
 def record():
     if request.method == 'POST':
         userid = request.form['userid']
         score = request.form['score']
         menuname = request.form['menuname']
-        menucal = request.form['menucal']  
+        menucal = request.form['menucal']
         
     con =sqlite3.connect('MISProject_database.db')
     cur = con.cursor()
-    cur.execute(f"INSERT INTO Record_table (`user_id`, `menuname`,`menucal`, `score`, `finish_time`) VALUES( '{userid}','{menuname}','{menucal}','{score}', datetime('now'))")
+    cur.execute(f"INSERT INTO Record_table (`user_id`, `menuname`,`menucal`, `score`, `finish_time`) VALUES( 2,'{menuname}','{menucal}','{score}', datetime('now'))")
     con.commit()
     con.close()
     return "successful insert"
