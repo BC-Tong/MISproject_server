e821ef9c28749f6914c62854262caf0b5002b17c
BC-Tong <85863509+BC-Tong@users.noreply.github.com>
2022-07-27 23:04:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -198,6 +198,7 @@ def testunity():
         
 if __name__ == "__main__":
     app.config['JSON_AS_ASCII'] = False
+    app.config['JSONIFY_MIMETYPE'] = "application/json;charset=utf-8"  
     app.run(host='127.0.0.1', port=5050, debug=True)
 
 
