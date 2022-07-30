2785087243b223f7c862c18e22e3611223ff1ea6
BC-Tong <85863509+BC-Tong@users.noreply.github.com>
2022-07-30 14:48:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -248,16 +248,15 @@ def printrecord():
     con.close()
     
     if result:
-        '''
-        for i in len(querydata.fetchall()):
+        for i in len(result):
             for row in result:
                 array1[i][0] = row[5]
                 array1[i][1] = row[2]
                 array1[i][2] = row[3]
                 array1[i][3] = row[4]
         return array1 
-        '''
-        return json.dumps(result, ensure_ascii=False).encode('utf8')
+        
+        #return json.dumps(result, ensure_ascii=False).encode('utf8')
     else:
         return "DB do not have data"    
     
